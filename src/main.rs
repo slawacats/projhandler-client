08b9697c5a4a41e6c833c7dd7e0d0c1b0c99932c
nd1 <slawacats@gmail.com>
2025-12-25 20:02:02 +0300

    2.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-//projhandler-client
+use std::env;
+
+
+fn load() {
+    println!("Загрузка проекта...")
+}
+
+fn save() {
+    println!("Сохранение проекта...")
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let command = &args[1];
+        match command.as_str() {
+            "load" => load(),
+            "save" => save(),
+            _ => println!("Неизвестная команда: {}", command),
+        }
+    } else {
+        println!("Использование: projhandler <load|save>");
+    }
 }
